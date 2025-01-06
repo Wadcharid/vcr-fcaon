1bfdc6d0c92933f51bfa1802094ff74f6b488aa1
Wadcharid <fcaonz@gmail.com>
2025-01-06 18:46:07 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
             <option value="https://wadcharid.github.io/vcr-fcaon/pace3.html">5.VCR Weekly Plan</option>
             <option value="https://wadcharid.github.io/vcr-fcaon/load1.html">6.COROS Load</option>
             <option value="https://wadcharid.github.io/vcr-fcaon/training1.html">7.Training Program</option>
-            <option value="https://wadcharid.github.io/vcr-fcaon/load1.html">8.คำนวณเวลาวิ่งรวม</option>
+            <option value="https://wadcharid.github.io/vcr-fcaon/time1.html">8.คำนวณเวลาวิ่งรวม</option>
         </select>
     `;
     document.getElementById("menu-container").innerHTML = menu;
